e86cb0ebe30bf9544156f7f173c0b6c80596ee06
taka-2120 <yu_tkhs@icloud.com>
2025-03-03 13:51:45 +0900

    Apply Session as return type of AuthRepository functions

--- InsightHub/Repository/AuthRepository.swift
+++ InsightHub/Repository/AuthRepository.swift
@@ -1,9 +1,15 @@
+import Auth
+
 class AuthRepository {
-    static func logIn(email: String, password: String) async throws {
+    static func logIn(email: String, password: String) async throws -> Session {
         try await Supabase.client.auth.signIn(email: email, password: password)
     }
 
-    static func signUp(email: String, password: String) async throws {
-        try await Supabase.client.auth.signUp(email: email, password: password)
+    static func signUp(email: String, password: String) async throws -> Session {
+        let response = try await Supabase.client.auth.signUp(email: email, password: password)
+        guard let session = response.session else {
+            throw AuthError.noSession
+        }
+        return session
     }
 }
